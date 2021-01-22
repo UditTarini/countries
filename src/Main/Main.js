8382f6e677237b14f838841332e2180d327ec93e
UditTarini <udit.tarini937@gmail.com>
2021-01-22 20:42:07 +0530

    collapse button

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -104,7 +104,17 @@ const Main = () => {
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
           Countries
         </a>
-
+        <button
+          class="navbar-toggler position-absolute d-md-none collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <input
           className="form-control form-control-dark w-100"
           type="text"
